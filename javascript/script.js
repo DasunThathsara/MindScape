0d44e20e0824f6be1f488201ded7ab1d6e5e7cc2
Dasun Thathsara <dasun.thathsara.sri@gmail.com>
2023-04-04 20:27:55 +0530

    Update reading section

--- javascript/script.js
+++ javascript/script.js
@@ -8,3 +8,12 @@ function redirectToPage() {
         alert("Incorrect user name or password!");
     }
 }
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
