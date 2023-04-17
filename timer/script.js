40d82b38a33eb1b5f53589487904daa235e2e5fb
Dasun Thathsara <dasun.thathsara.sri@gmail.com>
2023-04-17 13:54:28 +0530

    Add timer to yogaSection

---
+++ timer/script.js
@@ -0,0 +1,57 @@
+var seconds;
+var temp;
+
+function countdown() {
+    time = document.getElementById('countdown').innerHTML;
+    timeArray = time.split(':')
+    seconds = timeToSeconds(timeArray);
+    if (seconds == '') {
+        temp = document.getElementById('countdown');
+        temp.innerHTML = "00:00:00";
+
+        myFunction2();
+        return;
+    }
+    seconds--;
+    temp = document.getElementById('countdown');
+    temp.innerHTML = secondsToTime(seconds);
+    timeoutMyOswego = setTimeout(countdown, 1000);
+}
+
+function timeToSeconds(timeArray) {
+    var minutes = (timeArray[0] * 60) + (timeArray[1] * 1);
+    var seconds = (minutes * 60) + (timeArray[2] * 1);
+    return seconds;
+}
+
+function secondsToTime(secs) {
+    var hours = Math.floor(secs / (60 * 60));
+    hours = hours < 10 ? '0' + hours : hours;
+    var divisor_for_minutes = secs % (60 * 60);
+    var minutes = Math.floor(divisor_for_minutes / 60);
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var divisor_for_seconds = divisor_for_minutes % 60;
+    var seconds = Math.ceil(divisor_for_seconds);
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    return hours + ':' + minutes + ':' + seconds;
+}
+
+
+
+
+function myFunction1() {
+    var element = document.querySelector('.timer');
+    element.classList.toggle("timer_d");
+
+    var element = document.querySelector('.timer_btn');
+    element.classList.toggle("timer_btn_d");
+
+    countdown();    
+ }
+
+function myFunction2() {
+    var element = document.querySelector('.msg');
+    element.classList.toggle("msg_d");
+}
+
+ 
